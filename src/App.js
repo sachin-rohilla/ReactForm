6e8e1943f6f77935fe9dda08cdfa3e10e32b7f25
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-08 17:57:26 +0530

    Add Dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Login from "./Components/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
+  let isLogin=false
   return (
     <BrowserRouter>
     <Navbar/>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={isLogin?<Dashboard/>:<Navigate to="/login" replace/>}/>
       </Routes>
     </BrowserRouter>
   );
