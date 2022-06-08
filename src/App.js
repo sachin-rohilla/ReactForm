69779fcaf8d0f4f31388bd670cce24699592420d
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-08 15:07:00 +0530

    add new components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
-import React, { useState } from "react";
+import Login from "./Components/Login";
 
 function App() {
- const[values,setValues]=useState({
-   name:"",
-   password:""
- })
- const[login,setLogin]=useState(false);
- const[valid,setValid]=useState(false);
- const nameHandler=(e)=>{
-   setValues({...values,name:e.target.value})
- }
-  const passwordHandler = (e) => {
-    setValues({ ...values, password: e.target.value });
-  };
-  const loginHanlder=(e)=>{
-    if(values.name && values.password){
-      setValid(true);
-    }
-    e.preventDefault();
-
-    setLogin(true)
-    
-  }
-  
+ 
   return (
-    <div className="form-container">
-      <form className="login-form" onSubmit={loginHanlder}>
-        {login&&valid ? (
-          <div className="login-message">Thank you for login</div>
-        ) : (
-          ""
-        )}
-        <input
-          className="form-input"
-          placeholder="Enter Your Name"
-          name="Name"
-          value={values.name}
-          onChange={nameHandler}
-        />
-      {login&&!values.name?<span>Please enter your name</span>:''}
-        <input
-          type="password"
-          className="form-input"
-          placeholder="Enter Your Password"
-          value={values.password}
-          onChange={passwordHandler}
-        />
-        {login &&!values.password?<span>Please enter your password</span>:''}
-        <button className="form-input">Login</button>
-      </form>
-    </div>
+    <Login/>
   );
 }
 
