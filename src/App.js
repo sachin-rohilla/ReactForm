055aea7203395dd18c6422561116d2daa5a3c87f
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-08 16:28:35 +0530

    Routing add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Login from "./Components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
- 
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
