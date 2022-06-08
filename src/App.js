8f048b0a9477196cff6518dad2538be23d2d50fb
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-08 17:28:03 +0530

    Add Navlink

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Login from "./Components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+        <NavLink to="/login"><button>Login here</button></NavLink>
+    </div>
+  )
+}
+
+export default Navbar
